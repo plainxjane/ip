208385fc88e2b8d1510fa606611969b3ba4c2c1b
jane <baldeagle@janes-MacBook-Air-3.local>
2026-08-28 18:59:04 +0800

    Set up Gradle to run chatbot

--- src/main/java/JellyException.java
+++
@@ -1,7 +0,0 @@
-public class JellyException extends Exception {
-
-    /* a custom exception class JellyException to represent exceptions specific to Jelly chatbot */
-    public JellyException(String message) {
-        super(message);
-    }
-}
